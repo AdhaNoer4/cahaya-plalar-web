f052d6a30feb871d715381480e4a3606a7b38754
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2026-02-12 08:52:32 +0700

    Add filter Categories

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import { products } from "../data/products";
 import ProductCard from "./ProductCard";
+import { button } from "framer-motion/client";
 
 export default function ProductGrid() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const categories = ["All", "Sembako", "Kebutuhan Rumah Tangga"];
+
+  const filterredProducts = selectedCategory === "All" ? products : products.filter((product) => product.category === selectedCategory);
+
   return (
     <section className="py-20 bg-gray-200">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl font-bold mb-10 text-center">Produk Unggulan</h2>
+
+        {/* Filter Buttons */}
+        <div className="flex justify-center gap-4 mb-10">
+          {categories.map((cat) => (
+            <button
+              key={cat}
+              onClick={() => setSelectedCategory(cat)}
+              className={`px-4 py-2 rounded-lg border
+              ${selectedCategory === cat ? "bg-blue-600 text-white" : "bg-white text-gray-700"}`}
+            >
+              {cat}
+            </button>
+          ))}
+        </div>
+
+        {/* Product Grid */}
+
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {products.map((product) => (
+          {filterredProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
--- src/data/products.js
+++ src/data/products.js
@@ -1,23 +1,37 @@
 export const products = [
-    {
-        id: 1,
-        name: "Beras 5kg",
-        price: 65000,
-        image: "https://via.placeholder.com/300",
-        category: "Sembako"
-    },
-    {
-        id: 2,
-        name: "Minyak Goreng 1L",
-        price: 18000,
-        image: "https://via.placeholder.com/300",
-        category: "Sembako"
-    },
-    {
-        id: 3,
-        name: "Gula Pasir 1kg",
-        price: 12000,
-        image: "https://via.placeholder.com/300",
-        category: "Sembako"
-    }
-]
+  {
+    id: 1,
+    name: "Beras 5kg",
+    price: 65000,
+    image: "https://placehold.co/600x400",
+    category: "Sembako",
+  },
+  {
+    id: 2,
+    name: "Minyak Goreng 1L",
+    price: 18000,
+    image: "https://placehold.co/600x400",
+    category: "Sembako",
+  },
+  {
+    id: 3,
+    name: "Gula Pasir 1kg",
+    price: 12000,
+    image: "https://placehold.co/600x400",
+    category: "Sembako",
+  },
+  {
+    id: 4,
+    name: "Sabun Mandi",
+    price: 5000,
+    image: "https://placehold.co/600x400",
+    category: "Kebutuhan Rumah Tangga",
+  },
+  {
+    id: 5,
+    name: "Sampo 200ml",
+    price: 15000,
+    image: "https://placehold.co/600x400",
+    category: "Kebutuhan Rumah Tangga",
+  },
+];
